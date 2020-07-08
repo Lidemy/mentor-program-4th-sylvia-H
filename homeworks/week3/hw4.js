32611ccb678b9d67d29bd513f551fdd5c823811d
sylvia-H <57402730+sylvia-H@users.noreply.github.com>
2020-07-08 12:56:54 +0800

    week3 hw4 pre-commit

--- homeworks/week3/hw4.js
+++ homeworks/week3/hw4.js
@@ -11,13 +11,14 @@ rl.on('line', (line) => {
 });
 
 function solve(input) {
+  const str = input[0];
   let res = '';
 
-  for (let i = input.length - 1; i >= 0; i -= 1) {
-    res += input[i];
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    res += str[i];
   }
 
-  if (res === input) {
+  if (res === str) {
     console.log('True');
   } else {
     console.log('False');
